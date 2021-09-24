ffb2358b90a2938f01aa7a5c1097fda9ddd37f8d
AymanAlshiekh <aymanalshiekh1@gmail.com>
2021-09-24 19:29:03 +0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  
+});
+
+function openNav() {
+  document.getElementById("dashboard-side-nav").style.width = "250px";
+}
+function closeNav() {
+  document.getElementById("dashboard-side-nav").style.width = "0";
+}
